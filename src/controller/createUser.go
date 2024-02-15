a34fced557066809a82fb5664c5ed445fdf48cbc
EnzoGamaDs <enzo.gamads@gmail.com>
2024-02-15 15:33:25 -0300

    feat: erros personalizados

--- src/controller/createUser.go
+++ src/controller/createUser.go
@@ -4,19 +4,16 @@ import (
 	"fmt"
 	"net/http"
 
-	"github.com/EnzoGamaDS/Go-Platform-Project/src/configuration/rest_err"
+	"github.com/EnzoGamaDS/Go-Platform-Project/src/configuration/validation"
 	"github.com/EnzoGamaDS/Go-Platform-Project/src/controller/model/request"
 	"github.com/gin-gonic/gin"
 )
 
 func CreateUser(c *gin.Context) {
-
 	var userRequest request.UserRequest
 
 	if err := c.ShouldBindJSON(&userRequest); err != nil {
-		restErr := rest_err.NewBadRequestError(
-			fmt.Sprintf("there are some incorrect fields, errors=%s\n", err.Error()),
-		)
+		restErr := validation.ValidateUserError(err)
 		c.JSON(restErr.Code, restErr)
 		return
 	}
