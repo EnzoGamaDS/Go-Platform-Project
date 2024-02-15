c55c270447a4ab52c2c3385734c00ceadd0884cf
EnzoGamaDs <enzo.gamads@gmail.com>
2024-02-15 16:10:04 -0300

    feat: adicionando logs ao projeto

--- go.mod
+++ go.mod
@@ -23,6 +23,8 @@ require (
 	github.com/pelletier/go-toml/v2 v2.1.1 // indirect
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	github.com/ugorji/go/codec v1.2.12 // indirect
+	go.uber.org/multierr v1.11.0 // indirect
+	go.uber.org/zap v1.26.0 // indirect
 	golang.org/x/arch v0.7.0 // indirect
 	golang.org/x/crypto v0.19.0 // indirect
 	golang.org/x/net v0.21.0 // indirect
--- src/controller/createUser.go
+++ src/controller/createUser.go
@@ -1,22 +1,46 @@
 package controller
 
 import (
-	"fmt"
 	"net/http"
 
+	"github.com/EnzoGamaDS/Go-Platform-Project/src/configuration/logger"
 	"github.com/EnzoGamaDS/Go-Platform-Project/src/configuration/validation"
 	"github.com/EnzoGamaDS/Go-Platform-Project/src/controller/model/request"
+	"github.com/EnzoGamaDS/Go-Platform-Project/src/controller/model/response"
 	"github.com/gin-gonic/gin"
+	"go.uber.org/zap"
 )
 
 func CreateUser(c *gin.Context) {
+	logger.Info("Init create user controller",
+		zap.String(
+			"journey",
+			"CreateUser",
+		))
 	var userRequest request.UserRequest
 
 	if err := c.ShouldBindJSON(&userRequest); err != nil {
+		logger.Error("Error trying to validate user info", err,
+			zap.String(
+				"journey",
+				"CreateUser",
+			))
 		restErr := validation.ValidateUserError(err)
 		c.JSON(restErr.Code, restErr)
 		return
 	}
-	fmt.Println(userRequest)
-	c.JSON(http.StatusOK, gin.H{"user": userRequest})
+	response := response.UserResponse{
+		ID:       "test",
+		Email:    userRequest.Email,
+		Name:     userRequest.Name,
+		Password: userRequest.Password,
+		Age:      userRequest.Age,
+	}
+
+	logger.Info("User created successfully",
+		zap.String(
+			"journey",
+			"CreateUser",
+		))
+	c.JSON(http.StatusOK, response)
 }
